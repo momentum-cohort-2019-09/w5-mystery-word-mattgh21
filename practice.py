994cf3c62bd15d40b519f8e177262202efc986c7
Matt Gouchoe-Hanas <mattgh21@gmail.com>
2019-10-10 11:41:21 -0400

    adds evalution of user input, adds loop through letter in letters_guessed, displays appended letters on '_'

--- practice.py
+++ practice.py
@@ -1,6 +1,6 @@
 import random
 
-# def open_and_read(file):
+
 
 with open ("words.txt") as my_file:
     my_file = my_file.readlines()
@@ -8,52 +8,69 @@ with open ("words.txt") as my_file:
     stripped_words = []
     for word in my_file:
         word = word.strip('\n')
-      
+    
         stripped_words.append(word)
-
-  
 
     word_to_guess = random.choice(stripped_words)
 
-    print("Letters in word: " + str(len(word_to_guess)))
+# print(f'The word to guess is {word_to_guess}')
+# print(word_to_guess[1])
+# print(len(word_to_guess))
 
 
+guesses_left = 8
+letters_guessed = []
 
+print("Letters in word: " + str(len(word_to_guess)))
+
+    
+
+while guesses_left > 0:
+    print(f"You have {guesses_left} guesses left")
     guess_letter = input("Guess a letter: ")
-    if len(guess_letter) > 1 or len(guess_letter) < 1:
+    word_display = []
+    print(letters_guessed)
+
+    if len(guess_letter) != 1:
         print('Must be one letter')
+    elif not guess_letter.isalpha():
+        print('Must be a letter')
+    elif guess_letter in letters_guessed:
+        print('You already guessed that') 
     else:
+        letters_guessed.append(guess_letter)
         for letter in word_to_guess:
-            if guess_letter != letter in word_to_guess:
-                  print("guess again")
-            if guess_letter == letter:
-                print(guess_letter)
+            # print(letter)
+                 
+            if letter in letters_guessed:
+                word_display.append(letter)
+            else:
+                word_display.append('_')
+                 
             
+        # print(f'word display is {word_display}')    
+        print(' '.join(word_display))
+        guesses_left -= 1
+        
 
 
+# if guesses_left = 0:      
+    # print('Sorry, you ran out of guesses. You lose.')
+print(f"The word was {word_to_guess}")
 
-            # elif guess_letter != letter:
-            #     print("guess again!")
 
     
-    
 
 
-    # print(guess_letter)
+
+
+
 
 
     # if random.choice(stripped_words) == guess_word:
     #     print('you guessed it!')
     # else:
     #     print("try again!")
-
-
-
-    # for letter in word_to_guess:
-    #     if guess_letter == letter:
-    #         print(guess_letter)
-    #     else:
-    #         print("try again!")
 
   
 
